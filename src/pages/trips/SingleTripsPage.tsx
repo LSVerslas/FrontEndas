7bb1d9c3d49f84109232db2d10e6bc14f89a58c3
Sakalis <lsverslas@gmail.com>
2024-03-14 17:35:36 +0200

    Pages

---
+++ src/pages/trips/SingleTripsPage.tsx
@@ -0,0 +1,12 @@
+//
+
+export default function SingleTripsPage() {
+  return (
+    <div>
+        <div className="container">
+            <h1 className="display-2">SingleTripsPage</h1>
+            <p>Welcome to our SingleTripsPage</p>
+        </div>
+    </div>
+  );
+}
