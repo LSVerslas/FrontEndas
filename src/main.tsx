77d1cdcc7c0ad5cffd3a27db51012783b08d8c4c
Sakalis <lsverslas@gmail.com>
2024-03-14 16:36:52 +0200

    add bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './styles/index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
     <App />
-  //* </React.StrictMode>,
-)
+  // </React.StrictMode>,
+);
