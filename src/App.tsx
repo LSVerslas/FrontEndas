0ac5e5e25f305f2a8fec0a76df7a10f1989f19d7
Sakalis <lsverslas@gmail.com>
2024-03-14 17:40:53 +0200

    Trip

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './styles/App.css';
 
 import Header from "./components/layout/Header";
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import TripsPage from './pages/trips/TripsPage';
+import SingleTripsPage from './pages/trips/SingleTripsPage';
+import CountriesPage from './pages/CountriesPage';
 
 export default function App() {
   return (
     <div className="">
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-      </Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/trips" element={<TripsPage />} />
+        <Route path="/trips/:tripId" element={<SingleTripsPage />} />
+        <Route path="/countries" element={<CountriesPage />} />
+      </Routes> 
     </div>
   );
 }
