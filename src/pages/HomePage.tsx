9d3a8226206d545450ce569ad79f61fc6f8d5bf0
Sakalis <lsverslas@gmail.com>
2024-03-21 10:48:57 +0200

    src change

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,10 +3,10 @@
 export default function HomePage() {
   return (
     <div>
-        <div className="container">
-            <h1 className="display-2">HomePage</h1>
-            <p>Welcome to our HomePage</p>
-        </div>
+      <div className='container'>
+        <h1 className='display-2'>HomePage</h1>
+        <p>Welcome to our HomePage</p>
+      </div>
     </div>
   );
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,17 +1,24 @@
 export type TripObjType = {
-    id: number;
-    name: string;
-    date: string;
-    country: string;
-    city: string;
-    rating: number;
-    description: string;
-    price: number;
-    user_id: number;
-    image_main: string;
-    images_1?: string;
-    images_2?: string;
-    images_3?: string;
-  };
-  
-  export type TripObjTypeNoId = Omit<TripObjType, 'id'>;
+  id: number;
+  name: string;
+  date: string;
+  country: string;
+  city: string;
+  rating: number;
+  description: string;
+  price: number;
+  user_id: number;
+  image_main: string;
+  images_1?: string;
+  images_2?: string;
+  images_3?: string;
+};
+
+export type TripObjTypeNoId = Omit<TripObjType, 'id'>;
+
+export type UserObjType = {
+  id?: number;
+  name?: string;
+  email: string;
+  password: string;
+};
